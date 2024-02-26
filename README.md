820dc8cffad7af4d57e8badaaef964be6a235816
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-26 22:09:34 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # End-to-End-NLP-Project-Implementation
+
+## How to run?
+
+```bash
+conda create -p env python=3.8 -y
+```
+```bash
+conda activate env/
+```
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Dr. TEJAS. J",
+    author_email="tejas05in@yahoo.co.in",
+    packages=find_packages(),
+    install_requires=[],
+)
